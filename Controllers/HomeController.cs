63c85f084ca4bee79029db7cd61d5f65bcd46436
thanhdmce160530 <minhthanhgn23@gmail.com>
2022-07-06 16:29:26 +0700

    hong

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,26 +3,22 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using FEBook.DataAccess.Repository;
 using FEBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 
-namespace EbookProject.Controllers
+namespace FEBook.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
+        // public HomeController(ILogger<HomeController> logger)
+        // {
+        //     _logger = logger;
+        // }
 
         public IActionResult Privacy()
         {
@@ -34,5 +30,17 @@ namespace EbookProject.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        IBookRepository bookRepository = null;
+        public HomeController() => bookRepository = new BookRepository();
+        public async Task<ActionResult> Index(string searchString) {
+            var BookList = bookRepository.GetBooks();
+            var searchBook = from book in BookList select book;
+            if(!String.IsNullOrEmpty(searchString)){
+                 searchBook = searchBook.Where(c => c.BookName!.Contains(searchString));   
+
+            }
+            return View(await Task.FromResult(searchBook.ToList()));
+
     }
+  }
 }
